a2d0eb420e8ddfbf400ba21c0eeb3a5b9527c34f
yhsj0919 <1130402124@qq.com>
2020-11-25 16:57:02 +0800

    测试Ktor

--- src/xyz/yhsj/ktor/entity/permission/SysPermission.kt
+++ src/xyz/yhsj/ktor/entity/permission/SysPermission.kt
@@ -5,6 +5,7 @@ import org.litote.kmongo.Id
 import xyz.yhsj.ktor.entity.common.BaseEntity
 import xyz.yhsj.ktor.validator.ValidationGroup
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class SysPermission(
     @field:NotBlank(message = "ID不可为空", groups = [ValidationGroup.Update::class, ValidationGroup.Delete::class])
@@ -17,20 +18,20 @@ data class SysPermission(
     val name: String? = null,
 
     //类型，0，菜单，1，接口
-    @field: NotBlank(message = "类型不可为空", groups = [ValidationGroup.Add::class])
-    val type: Int = 0,
+    @field: NotNull(message = "类型不可为空", groups = [ValidationGroup.Add::class])
+    val type: Int? = null,
 
-    @field: NotBlank(message = "权重不可为空", groups = [ValidationGroup.Add::class])
-    val weight: Int = 0,
+    @field: NotNull(message = "权重不可为空", groups = [ValidationGroup.Add::class])
+    val weight: Int? = null,
 
     @field: NotBlank(message = "路径不可为空", groups = [ValidationGroup.Add::class])
-    val path: Int? = 0,
+    val path: String? = null,
 
     val icon: Int? = 0xe867,
 
-    val enable: Boolean = true,
+    val enable: Boolean? = true,
 
-    val child: List<SysPermission>? = null
+    var child: List<SysPermission>? = null
 
 ) : BaseEntity()
 
--- src/xyz/yhsj/ktor/service/PermissionService.kt
+++ src/xyz/yhsj/ktor/service/PermissionService.kt
@@ -1,5 +1,6 @@
 package xyz.yhsj.ktor.service
 
+import org.litote.kmongo.Id
 import org.litote.kmongo.coroutine.CoroutineClient
 import xyz.yhsj.ktor.auth.AppSession
 import xyz.yhsj.ktor.dbName
@@ -11,19 +12,21 @@ import xyz.yhsj.ktor.ext.getCollection
  * 权限，菜单
  */
 class PermissionService(private val db: CoroutineClient) {
-    private val permission by lazy { db.getCollection<SysPermission>(dbName) }
+    private val permissionDB by lazy { db.getCollection<SysPermission>(dbName) }
 
     /**
      * 获取权限
      */
     suspend fun getPermission(params: SysPermission, sessions: AppSession): Any {
 
-        val result = permission.find().toList()
+        val result = permissionDB.find().toList()
 
-//        result.filter { it.parent == null }.map {=> it.child=result.filter { it.parent==it } }
+        val tree = getTree(result, null)
+        return CommonResp.success(data = tree)
+    }
 
-
-        return CommonResp.success(data = result)
+    private fun getTree(list: List<SysPermission>, parent: Id<SysPermission>?): List<SysPermission> {
+        return list.filter { it.parent == parent }.map { item -> item.child = getTree(list, item.id);item }.toList()
     }
 
     /**
@@ -31,6 +34,16 @@ class PermissionService(private val db: CoroutineClient) {
      */
     suspend fun addPermission(params: SysPermission, sessions: AppSession): Any {
 
-        return CommonResp.success()
+        val data = params.copy(id = null, enable = true, child = null)
+        data.deleted = 0
+        data.creatorId = sessions.user?.id
+        data.creator = null
+        data.company = null
+        permissionDB.insertOne(data)
+
+
+        val result = permissionDB.find().toList()
+        val tree = getTree(result, null)
+        return CommonResp.success(data = tree)
     }
 }
