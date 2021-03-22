c7825d92274f1cb63db9178770a51714129cec2e
yhsj0919 <1130402124@qq.com>
2021-03-22 15:56:56 +0800

    测试ktor

--- src/main/kotlin/xyz/yhsj/ktor/Application.kt
+++ src/main/kotlin/xyz/yhsj/ktor/Application.kt
@@ -132,10 +132,7 @@ fun Application.module(testing: Boolean = false) {
     }
     //拦截器
     intercept(ApplicationCallPipeline.Call) {
-
         println("host:" + call.request.host() + ":" + call.request.port() + call.request.path())
-
-
         //可以根据随机数拦截每一次请求，防止该cookie跳跃请求
 //        println(">>>>>>请求随机数>>>>>>>" + call.request.cookies.rawCookies["random"])
 //        val random = (Math.random() * 100000).toInt()
---
+++ src/test/kotlin/xyz/yhsj/ApplicationTest.kt
@@ -0,0 +1,19 @@
+package xyz.yhsj
+
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import xyz.yhsj.ktor.module
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
+    }
+}
